4e451e77cbe929b643a2a75fed6ae18cf11f066f
Timna Schulze <Timna.lansel@gmail.com>
2023-02-08 20:38:51 -0800

    WIP: making a function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Utility Logic
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+  event.preventDefault();
+  };
+};
+
+// Business Logic
+
+let cCValidator = (number) {
+  for (let i = 0; i <= number.length; i++) {
+    if number[i] === 
+  }
+}
+
